55e0f5d841efd8b2f7dc560af93aaeaa8161b0b4
Suuyshi <sarah.elsherbinyy@gmail.com>
2023-09-29 15:19:19 +0300

    working on user model

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user,
   });
   product
     .save()
@@ -76,6 +77,9 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select("title price -_id")
+    //.populate("userId", "name")
+    .populate("userId")
     .then((products) => {
       res.render("admin/products", {
         prods: products,
